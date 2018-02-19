79da3c150a1581e92bafefc88715893ca4b36b57
Travis Clarkson <tk1667@gmail.com>
2018-02-19 03:43:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(string)
-  array = string.split
-  reverse_array = []
-  array.each {|i| reverse_array << i.reverse}
-  reverse_array.join(" ")
-end
+# def reverse_each_word(string)
+#   array = string.split
+#   reverse_array = []
+#   array.each {|i| reverse_array << i.reverse}
+#   reverse_array.join(" ")
+# end
 
 def reverse_each_word(string)
   array = string.split
